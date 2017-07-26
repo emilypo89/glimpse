8f374c394d32a8eb9eea48d62a9bbc1c28fd7472
emilypo89 <emilypo89@gmail.com>
2017-07-26 12:47:43 -0400

    development branch

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 // Require Click schema
 var Event = require("./models/event");
@@ -21,6 +23,11 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("build"));
 
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 // -------------------------------------------------
 
 // MongoDB configuration (Change this URL to your own DB)
