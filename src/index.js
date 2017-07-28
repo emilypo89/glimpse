176ae0ddd2522b31a869ea093822403f63088120
Nikki Jedic <nikki.jedic@gmail.com>
2017-07-28 14:00:31 -0400

    Dashboard styled

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import Main from './main';
+import Dashboard from './dashboard';
 import './index.css';
 
 ReactDOM.render(
 	<BrowserRouter>
-	<Main />
+	<Dashboard />
 		
 	</BrowserRouter>, document.getElementById("root"));
