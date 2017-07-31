09be11e8d5128c95332bf662f6d59f85b5f1d811
Adam Kwan <adamkwan@Adams-MacBook.local>
2017-07-31 13:31:23 -0400

    landing page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Landing from './components/views/landing'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-const divStyle = {
-	height: 600
-}
+
 
 ReactDOM.render(
-	<BrowserRouter style={divStyle}>
-		<App />
+	<BrowserRouter >
+		<Landing />
 	</BrowserRouter>,
 	document.getElementById('root')
 )
